24183c4f715fcc3630667de3ef0846c48fcead05
Deepak Kapoor <nk93555@gmail.com>
2020-05-31 12:28:23 +0530

    first commit

---
+++ client/build/precache-manifest.79ae9203f36b708ed3af3e90d0e75726.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d82e5ac591482a08825de7229e9fbb9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c7d0beceee789ec91da",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "3a322878f6fc00ec58eb",
+    "url": "/static/css/main.e5551816.chunk.css"
+  },
+  {
+    "revision": "5c7d0beceee789ec91da",
+    "url": "/static/js/2.7a077155.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/static/js/2.7a077155.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a322878f6fc00ec58eb",
+    "url": "/static/js/main.c4b36627.chunk.js"
+  },
+  {
+    "revision": "f100e7b4b657f977d754",
+    "url": "/static/js/runtime-main.6246c68b.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
