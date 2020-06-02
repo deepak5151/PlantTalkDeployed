35ffea87506a40b599dd46222d81f54e6265e5fb
Deepak Kapoor <nk93555@gmail.com>
2020-06-02 18:48:43 +0530

    PlantTalkDeployed

--- server.js
+++ server.js
@@ -29,6 +29,10 @@ app.use('/details', CarDetailsRouter);
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
 }
+app.use(express.static("client/build"));
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(port, () => {
     console.log(`Server is running at port ${port}`);
